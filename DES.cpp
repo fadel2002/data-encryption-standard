e8ac38d64bff032e3858dabc3c0e1ac02267174a
Fadel Pramaputra Maulana <72655591+fadel2002@users.noreply.github.com>
2023-10-25 13:47:30 +0700

    Update DES.cpp

--- DES.cpp
+++ DES.cpp
@@ -16,50 +16,50 @@ void hexInput(string key);
 
 class DES_Encryption{
 	const int pc_1[56] = {  
-                57 ,49 ,41 ,33 ,25 ,17 ,9  ,
-				1  ,58 ,50 ,42 ,34 ,26 ,18 ,
-				10 ,2  ,59 ,51 ,43 ,35 ,27 ,
-				19 ,11 ,3  ,60 ,52 ,44 ,36 ,
-				63 ,55 ,47 ,39 ,31 ,23 ,15 ,
-				7  ,62 ,54 ,46 ,38 ,30 ,22 ,
-				14 ,6  ,61 ,53 ,45 ,37 ,29 ,
-				21 ,13 ,5  ,28 ,20 ,12 ,4 
-				};
+		57 ,49 ,41 ,33 ,25 ,17 ,9  ,
+		1  ,58 ,50 ,42 ,34 ,26 ,18 ,
+		10 ,2  ,59 ,51 ,43 ,35 ,27 ,
+		19 ,11 ,3  ,60 ,52 ,44 ,36 ,
+		63 ,55 ,47 ,39 ,31 ,23 ,15 ,
+		7  ,62 ,54 ,46 ,38 ,30 ,22 ,
+		14 ,6  ,61 ,53 ,45 ,37 ,29 ,
+		21 ,13 ,5  ,28 ,20 ,12 ,4 
+		};
 
 	int num_leftShift[16] = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 }; 
 
 	const int pc_2[48] = {  
                 14 ,17 ,11 ,24 ,1  ,5  ,
-				3  ,28 ,15 ,6  ,21 ,10 ,
-				23 ,19 ,12 ,4  ,26 ,8  ,
-				16 ,7  ,27 ,20 ,13 ,2  ,
-				41 ,52 ,31 ,37 ,47 ,55 ,
-				30 ,40 ,51 ,45 ,33 ,48 ,
-				44 ,49 ,39 ,56 ,34 ,53 ,
-				46 ,42 ,50 ,36 ,29 ,32 
-				};
+		3  ,28 ,15 ,6  ,21 ,10 ,
+		23 ,19 ,12 ,4  ,26 ,8  ,
+		16 ,7  ,27 ,20 ,13 ,2  ,
+		41 ,52 ,31 ,37 ,47 ,55 ,
+		30 ,40 ,51 ,45 ,33 ,48 ,
+		44 ,49 ,39 ,56 ,34 ,53 ,
+		46 ,42 ,50 ,36 ,29 ,32 
+		};
 
 	const int IP_t[64] = { 	
-				58 ,50 ,42 ,34 ,26 ,18 ,10 ,2 ,  
-				60 ,52 ,44 ,36 ,28 ,20 ,12 ,4 ,
-				62 ,54 ,46 ,38 ,30 ,22 ,14 ,6 ,
-				64 ,56 ,48 ,40 ,32 ,24 ,16 ,8 ,
-				57 ,49 ,41 ,33 ,25 ,17 ,9  ,1 ,
-				59 ,51 ,43 ,35 ,27 ,19 ,11 ,3 ,
-				61 ,53 ,45 ,37 ,29 ,21 ,13 ,5 ,
-				63 ,55 ,47 ,39 ,31 ,23 ,15 ,7 
-				};
+		58 ,50 ,42 ,34 ,26 ,18 ,10 ,2 ,  
+		60 ,52 ,44 ,36 ,28 ,20 ,12 ,4 ,
+		62 ,54 ,46 ,38 ,30 ,22 ,14 ,6 ,
+		64 ,56 ,48 ,40 ,32 ,24 ,16 ,8 ,
+		57 ,49 ,41 ,33 ,25 ,17 ,9  ,1 ,
+		59 ,51 ,43 ,35 ,27 ,19 ,11 ,3 ,
+		61 ,53 ,45 ,37 ,29 ,21 ,13 ,5 ,
+		63 ,55 ,47 ,39 ,31 ,23 ,15 ,7 
+		};
 
 	const int E_t[48] = { 	
-				32 ,1  ,2  ,3  ,4  ,5  , 
-				4  ,5  ,6  ,7  ,8  ,9  ,
-				8  ,9  ,10 ,11 ,12 ,13 ,
-				12 ,13 ,14 ,15 ,16 ,17 ,
-				16 ,17 ,18 ,19 ,20 ,21 ,
-				20 ,21 ,22 ,23 ,24 ,25 ,
-				24 ,25 ,26 ,27 ,28 ,29 ,
-				28 ,29 ,30 ,31 ,32 ,1 
-				};
+		32 ,1  ,2  ,3  ,4  ,5  , 
+		4  ,5  ,6  ,7  ,8  ,9  ,
+		8  ,9  ,10 ,11 ,12 ,13 ,
+		12 ,13 ,14 ,15 ,16 ,17 ,
+		16 ,17 ,18 ,19 ,20 ,21 ,
+		20 ,21 ,22 ,23 ,24 ,25 ,
+		24 ,25 ,26 ,27 ,28 ,29 ,
+		28 ,29 ,30 ,31 ,32 ,1 
+		};
 
 	int S[8][4][16] = {                      
 		{
@@ -114,23 +114,23 @@ class DES_Encryption{
 
 	const int P[32] = { 	
                 16 ,7  ,20 ,21 ,
-				29 ,12 ,28 ,17 ,
-				1  ,15 ,23 ,26 ,
-				5  ,18 ,31 ,10 ,
-				2  ,8  ,24 ,14 ,
-				32 ,27 ,3  ,9  ,
-				19 ,13 ,30 ,6  ,
-				22 ,11 ,4  ,25 };
+		29 ,12 ,28 ,17 ,
+		1  ,15 ,23 ,26 ,
+		5  ,18 ,31 ,10 ,
+		2  ,8  ,24 ,14 ,
+		32 ,27 ,3  ,9  ,
+		19 ,13 ,30 ,6  ,
+		22 ,11 ,4  ,25 };
 
 	const int P_1[64] = { 	
                 40 ,8  ,48 ,16 ,56 ,24 ,64 ,32 ,
-				39 ,7  ,47 ,15 ,55 ,23 ,63 ,31 ,
-				38 ,6  ,46 ,14 ,54 ,22 ,62 ,30 ,
-				37 ,5  ,45 ,13 ,53 ,21 ,61 ,29 ,
-				36 ,4  ,44 ,12 ,52 ,20 ,60 ,28 ,
-				35 ,3  ,43 ,11 ,51 ,19 ,59 ,27 ,
-				34 ,2  ,42 ,10 ,50 ,18 ,58 ,26 ,
-				33 ,1  ,41 ,9  ,49 ,17 ,57 ,25 };
+		39 ,7  ,47 ,15 ,55 ,23 ,63 ,31 ,
+		38 ,6  ,46 ,14 ,54 ,22 ,62 ,30 ,
+		37 ,5  ,45 ,13 ,53 ,21 ,61 ,29 ,
+		36 ,4  ,44 ,12 ,52 ,20 ,60 ,28 ,
+		35 ,3  ,43 ,11 ,51 ,19 ,59 ,27 ,
+		34 ,2  ,42 ,10 ,50 ,18 ,58 ,26 ,
+		33 ,1  ,41 ,9  ,49 ,17 ,57 ,25 };
 
 	string shift_bit(string s, int n){
 		string k = "";
@@ -362,16 +362,16 @@ string asciiInput(string key){
     getchar();
     getline(cin, plain_txt); 
 
-	// Untuk input ascii maka semua string text diubah dulu menjadi hexa
+    // Untuk input ascii maka semua string text diubah dulu menjadi hexa
     hexa_text = ASCIItoHEX(plain_txt);
 
-	// Menentukan banyaknya iterasi yang dilakukan untuk menghitung semua panjang 
-	// string per 64 bit / 16 karakter
+    // Menentukan banyaknya iterasi yang dilakukan untuk menghitung semua panjang 
+    // string per 64 bit / 16 karakter
     iteration = (int)ceil((float)hexa_text.size()/16.0);
 
-	// Tambahkan padding jika jumlah hexa bukan merupakan kelipatan 16
-	// Contoh panjang hexa 14 sedangkan DES menerima 64 bit sehingga diperlukan 16 hexa
-	// maka ditambahkan dengan padding yaitu 00 sehingga hexa sekarang ialah 16
+    // Tambahkan padding jika jumlah hexa bukan merupakan kelipatan 16
+    // Contoh panjang hexa 14 sedangkan DES menerima 64 bit sehingga diperlukan 16 hexa
+    // maka ditambahkan dengan padding yaitu 00 sehingga hexa sekarang ialah 16
     string padding = "";
     for(int i=iteration*16-hexa_text.size(); i>0; i--){
         padding = padding + "0";
@@ -380,7 +380,7 @@ string asciiInput(string key){
 
     cout << "\nHexadec   : " << hexa_text << "\n";
 
-	// Encrypt hexa secara berurutan dengan panjang per 16 karakter atau 64 bit
+    // Encrypt hexa secara berurutan dengan panjang per 16 karakter atau 64 bit
     string temp = "";
     string result_encrypted = "";
     for(int i=0; i<iteration; i++){
@@ -393,7 +393,7 @@ string asciiInput(string key){
 
     cout << "Encrypted : " << result_encrypted << "\n";
 
-	// Decrypt hexa secara berurutan dengan panjang per 16 karakter atau 64 bit
+    // Decrypt hexa secara berurutan dengan panjang per 16 karakter atau 64 bit
     string result_decrypted = "";
     for(int i=0; i<iteration; i++){
         for (int j=i*16; j<(i+1)*16; j++){
@@ -416,13 +416,13 @@ void hexInput(string key){
 
     cin >> hexa_text;
 
-	// Menentukan banyaknya iterasi yang dilakukan untuk menghitung semua panjang 
-	// string per 64 bit / 16 karakter
+    // Menentukan banyaknya iterasi yang dilakukan untuk menghitung semua panjang 
+    // string per 64 bit / 16 karakter
     iteration = (int)ceil((float)hexa_text.size()/16.0);
 
-	// Tambahkan padding jika jumlah hexa bukan merupakan kelipatan 16
-	// Contoh panjang hexa 14 sedangkan DES menerima 64 bit sehingga diperlukan 16 hexa
-	// maka ditambahkan dengan padding yaitu 00 sehingga hexa sekarang ialah 16
+    // Tambahkan padding jika jumlah hexa bukan merupakan kelipatan 16
+    // Contoh panjang hexa 14 sedangkan DES menerima 64 bit sehingga diperlukan 16 hexa
+    // maka ditambahkan dengan padding yaitu 00 sehingga hexa sekarang ialah 16
     string padding = "";
     for(int i=iteration*16-hexa_text.size(); i>0; i--){
         padding = padding + "0";
@@ -431,7 +431,7 @@ void hexInput(string key){
 
     cout << "\nHexadec   : " << hexa_text << "\n";
 
-	// Encrypt hexa secara berurutan dengan panjang per 16 karakter atau 64 bit
+    // Encrypt hexa secara berurutan dengan panjang per 16 karakter atau 64 bit
     string temp = "";
     string result_encrypted = "";
     for(int i=0; i<iteration; i++){
@@ -444,7 +444,7 @@ void hexInput(string key){
 
     cout << "Encrypted : " << result_encrypted << "\n";
 
-	// Decrypt hexa secara berurutan dengan panjang per 16 karakter atau 64 bit
+    // Decrypt hexa secara berurutan dengan panjang per 16 karakter atau 64 bit
     string result_decrypted = "";
     for(int i=0; i<iteration; i++){
         for (int j=i*16; j<(i+1)*16; j++){
